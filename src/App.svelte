99e506333799d5b646d2ecb183bbf6a2faf4a04b
calebtgraves <calebtgraves@gmail.com>
2024-11-13 17:28:55 -0700

    got a header working

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,36 @@
 </script>
 
 <main>
-  <h1>Caleb Graves</h1>
+  <header>
+    <div class="container">
+      <div class="header-container">
+        <div>
+          <div class="title-line">
+            <h1>Caleb Graves</h1>
+            <h2>Software Developer</h2>
+          </div>
+          <div class="contact-line">
+            <h3>calebtgraves@gmail.com</h3>
+          </div>
+        </div>
+        <div>
+          <a href="https://www.github.com/calebtgraves" target="_blank">
+            <img class="contact-image" src="images/github.svg" alt="GitHub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/caleb-graves-02b2011a6/"
+            target="_blank"
+          >
+            <img
+              class="contact-image"
+              src="images/linkedin.png"
+              alt="LinkedIn"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
 </main>
 
 <style>
